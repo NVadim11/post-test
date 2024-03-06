62763bd6a5e4dcd21556b50643128a306cf8cad6
NVadim <palaginvad@gmail.com>
2024-03-06 16:12:35 +0200

    post test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
 import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 import '@solana/wallet-adapter-react-ui/styles.css'
@@ -6,9 +7,11 @@ import {
   SolflareWalletAdapter,
   TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets"
+import { clusterApiUrl } from '@solana/web3.js'
 import React, { useMemo } from 'react'
 import './App.css'
 import SolanaWalletButton from './btn'
+
 
 function App() {
   return (
@@ -21,9 +24,9 @@ function App() {
 export default App;
 
 const Context = ({children}) => {
-  // const network = WalletAdapterNetwork.Devnet;
-  // const endpoint = useMemo(() => clusterApiUrl(network), [network])
-  const endpoint = "http://localhost:3000"; // local cluster override
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  // const endpoint = "http://localhost:3000"; // local cluster override
 
   const wallets = useMemo( () => [
     new PhantomWalletAdapter(),
